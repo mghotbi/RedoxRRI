bcd7770cf1ff44fd717d00ac71449f763841d639
Mitra Ghotbi <mitra.ghotbi@gmail.com>
2026-02-22 16:45:53 +0100

    citation

---
+++ inst/CITATION.R
@@ -0,0 +1,15 @@
+citHeader("To cite RedoxRRI in publications use:")
+
+citEntry(
+  entry = "Manual",
+  title = "RedoxRRI: Holobiont Redox Resilience Index for Integrative Stress Biology",
+  author = personList(
+    person("Mitra", "Ghotbi", role = c("aut", "cre"),
+           comment = c(ORCID = "0000-0001-9185-9993")),
+    person("Marjan", "Ghotbi", role = "ctb",
+           comment = c(ORCID = "0000-0003-4655-6445"))
+  ),
+  year = "2026",
+  note = "R package version 0.99.0",
+  url = "https://github.com/mghotbi/RedoxRRI"
+)
